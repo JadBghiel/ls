d7acbe67992e4863fba79df37ec5abea6bc319e3
JadBGHIEL <jad.bghiel@epitech.eu>
2024-11-02 17:55:19 +0100

    add: header file and main file

---
+++ my_ls.c
@@ -0,0 +1,34 @@
+/*
+** EPITECH PROJECT, 2024
+** my_ls
+** File description:
+** recreate the ls command
+*/
+#include <stdio.h>
+#include "my_ls.h"
+
+int main(int argc, char **argv)
+{
+
+}
+
+void check_arguments()
+{
+
+}
+
+void set_option_flag()
+{
+// based on the argument, corresponds the arg and the struct var
+}
+
+int is_directory()
+{
+// check its a dir
+}
+
+void process_directory()
+{
+
+}
+
